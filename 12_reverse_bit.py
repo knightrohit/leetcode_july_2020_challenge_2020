16ebb20b3a311b1e7fe1c883b402034763c348cc
Rohit <knight.rohit@gmail.com>
2020-07-14 00:23:53 -0500

    using bit wise

--- 12_reverse_bit.py
+++ 12_reverse_bit.py
@@ -7,3 +7,12 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         out = bin(n)[2:][::-1]        
         return int(out+'0'*(32-len(out)), 2)
+
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        ans = 0
+        for i in range(32):
+            ans = (ans << 1) | (n & 1)
+            n >>= 1
+        return ans
