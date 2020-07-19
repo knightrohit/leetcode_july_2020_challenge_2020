c1fccf4b14d7c74dceee1bafee98b191bb82b47a
Rohit <knight.rohit@gmail.com>
2020-07-19 16:39:38 -0500

    using bit wise

--- 19_add_binary.py
+++ 19_add_binary.py
@@ -49,3 +49,17 @@ class Solution:
             out.append('1')
                 
         return ''.join(out[::-1])
+
+
+
+#Method 3
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        a, b = int(a,2), int(b,2)
+        
+        while b:
+            carry = a & b
+            a = a ^ b
+            b = carry << 1
+            
+        return bin(a)[2:]
